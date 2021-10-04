2aa71882026122252c9290981ecd87e031182c1e
Daniel Vo <daniel.vo@berkeley.edu>
2021-10-03 20:26:57 -0700

    fetch data properly ready to be added dynamically to site

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
+var forecast = $("#forecastBtn");
+
+// personal api key 
 var apiKey = "ff3dbe6a37f97b172f9338b03931d6bf";
-var city = "Oakland";
-
-var forecast = $("#forecastBtn")
 
 
+function foreCast(lat,lon) {
+	var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+	//fetching response from the api data 
+	fetch(requestUrl)
+	//We then convert the response into JSON and return the response
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function(data) {
+		console.log("data", data);
+	});
+}
 
-function requestWeather() {
+function findCoord() {
+	//reassigns the input value to city variable
+	var city = $('#cityInput').val();
+
 	//api url 
-	var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+	var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city +'&appid=' + apiKey;
 
 //fetching response from the api data 
 	fetch(requestUrl)
 	//We then convert the response into JSON and return the response
 	.then(function (response) {
-		console.log("response", response)
-		return response.json();
+		if (response.status >= 200 && response.status <= 299) {
+				return response.json();
+		} else {
+			alert('Please try again.');
+			throw Error(response.statusText);
+		}
 	})
 	.then(function(data) {
-		console.log("data", data)
+		console.log(data);
+		lat = data.coord.lat;
+		lon = data.coord.lon;
+		foreCast(lat,lon);
 	});
 }
 
 
-
-
-
-
-
-
-
-
-forecast.click(requestWeather);
-
+forecast.click(findCoord);
 
